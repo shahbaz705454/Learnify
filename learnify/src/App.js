5d1cdb5a37e803bf781a9778f17943ce498c2595
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-01-20 10:37:08 +0530

    Add Settings page with profile management components; implement date formatting utility

--- learnify/src/App.js
+++ learnify/src/App.js
@@ -10,6 +10,13 @@ import OpenRoute from './components/core/Auth/OpenRoute';
 import UpdatePassword from './Pages/UpdatePassword';
 import VerifyEmail from './Pages/VerifyEmail';
 import Contact from './Pages/Contact';
+import MyProfile from './components/core/Dashboard/MyProfile';
+import Dashboard from "./Pages/Dashboard"
+import PrivateRoute from './components/core/Auth/PrivateRoute';
+import Error from "./Pages/Error"
+import Settings from "./components/core/Dashboard/Settings";
+
+
 
 
 function App() {
@@ -17,6 +24,7 @@ function App() {
     <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
       <Navbar></Navbar>
       <Routes>
+
 
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
@@ -27,6 +35,44 @@ function App() {
         <Route path="update-password/:id" element={<OpenRoute><UpdatePassword /></OpenRoute>} />
         <Route path="forgot-password" element={<OpenRoute><ForgotPassword /> </OpenRoute>} />
         <Route path="verify-email" element={<OpenRoute><VerifyEmail /> </OpenRoute>} />
+
+        <Route
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+
+           <Route path="dashboard/Settings" element={<Settings />} />
+
+
+          {/*{
+            user?.accountType === ACCOUNT_TYPE.STUDENT && (
+              <>
+                <Route path="dashboard/cart" element={<Cart />} />
+                <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+              </>
+            )
+          }
+
+          {
+            user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+              <>
+                <Route path="dashboard/instructor" element={<Instructor />} />
+                <Route path="dashboard/add-course" element={<AddCourse />} />
+                <Route path="dashboard/my-courses" element={<MyCourses />} />
+                <Route path="dashboard/edit-course/:courseId" element={<EditCourse />} />
+
+              </>
+            )
+          } */}
+
+
+        </Route>
+
+        <Route path="*" element={<Error />} />
 
       </Routes>
 
